d41bcb1be02aac3f9a48f626fceb0410b2b529ca
bahagiga <baha.koyuncu@gmail.com>
2014-03-30 17:31:38 +0200

    Add NotString

---
+++ Conditionals/09 - NotString/09 - NotString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09___NotString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NotString("candy"));
+            Console.WriteLine(NotString("x"));
+            Console.WriteLine(NotString("not bad"));
+
+            Console.ReadLine();
+        }
+
+        /* Given a string, return a new string where "not " has been added to the front. 
+         * However, if the string already begins with "not", return the string unchanged.
+         * Hint: Look up how to use "SubString" in c# */
+
+        public static string NotString(string s)
+        {
+            if (s.Length > 2 && s.Substring(0, 3).Equals("not"))
+            {
+                return s;
+            }
+            return "not " + s;
+        }
+    }
+}
