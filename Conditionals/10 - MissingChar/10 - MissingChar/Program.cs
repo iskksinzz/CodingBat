b963f4b18f04680e240a08206f14764c2c7b9845
bahagiga <baha.koyuncu@gmail.com>
2014-03-30 17:51:23 +0200

    Add MissingChar

---
+++ Conditionals/10 - MissingChar/10 - MissingChar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___MissingChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MissingChar("kitten", 1));
+            Console.WriteLine(MissingChar("kitten", 0));
+            Console.WriteLine(MissingChar("kitten", 4));
+            
+            Console.ReadLine();
+        }
+
+        /* Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+         * The value of n will be a valid index of a char in the original string (Don't check for bad index).  */
+
+        public static string MissingChar(string str, int n)
+        {
+            return str.Remove(n, 1);
+        }
+    }
+}
