3824786b9c02f85c752ecf72ca54f33614ab9864
bahagiga <baha.koyuncu@gmail.com>
2014-04-03 20:54:24 +0200

    Add Multiple3or5

---
+++ Conditionals/14 - Multiple3or5/14 - Multiple3or5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14___Multiple3or5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiple3Or5(3));
+            Console.WriteLine(Multiple3Or5(10));
+            Console.WriteLine(Multiple3Or5(8));
+            Console.ReadLine();
+        }
+
+        /* Return true if the given non-negative number is a multiple of 3 or a multiple of 5. 
+         * Use the % "mod" operator */
+
+        public static bool Multiple3Or5(int number)
+        {
+            return number % 3 == 0 || number % 5 == 0;
+        }
+    }
+}
