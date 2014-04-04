db93148a1a9e54baa295afd7184331702cc095e2
bahagiga <baha.koyuncu@gmail.com>
2014-04-04 17:09:13 +0200

    Add HasTeen

---
+++ Conditionals/18 - HasTeen/18 - HasTeen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18___HasTeen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasTeen(13, 20, 10));
+            Console.WriteLine(HasTeen(20, 19, 10));
+            Console.WriteLine(HasTeen(20, 10, 12));
+            Console.ReadLine();
+        }
+
+        /* We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
+         * Given 3 int values, return true if 1 or more of them are teen. */
+
+        public static bool HasTeen(int a, int b, int c)
+        {
+            return (a > 12 && a < 20) || (b > 12 && b < 20) || (c > 12 && c < 20);
+        }
+    }
+}
