dfdaf591296a3088f4ac0ee13818739e4f92a55d
bahagiga <baha.koyuncu@gmail.com>
2014-04-07 19:07:18 +0200

    Add EEE.

---
+++ Conditionals/25 - EEEEEEE/25 - EEEEEEE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25___EEEEEEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GotE("Hello"));
+            Console.WriteLine(GotE("Heelle"));
+            Console.WriteLine(GotE("Heelele"));
+            Console.ReadLine();
+        }
+
+        /* Return true if the given string contains between 1 and 3 'e' chars. */
+
+        public static bool GotE(string str)
+        {
+            //var count = str.Split('e').Length - 1;
+
+            // LINQ
+            var count = str.Count(s => s == 'e');
+            return count <= 3;
+        }
+    }
+}
