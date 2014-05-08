e8bc8f68a6b1192dc8efd9da5213f92bf683c396
bahagiga <baha.koyuncu@gmail.com>
2014-05-08 21:44:34 +0200

    Add CanHazTable

---
+++ Logic/02 - CanHazTable/02 - CanHazTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02___CanHazTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanHazTable(5, 10));
+            Console.WriteLine(CanHazTable(5, 2));
+            Console.WriteLine(CanHazTable(5, 5));
+            Console.ReadLine();
+        }
+
+        /* You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness 
+         * of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. 
+         * The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. 
+         * If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that 
+         * if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe). */
+
+        public static int CanHazTable(int yourStyle, int dateStyle)
+        {
+            return (yourStyle <= 2 || dateStyle <= 2) ? 0 : (yourStyle >= 8 || dateStyle >= 8) ? 2 : 1;
+        }
+    }
+}
