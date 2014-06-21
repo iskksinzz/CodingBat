f4da6e98c93aea9eaa34e5c205233f87c2f381d9
bahagiga <baha.koyuncu@gmail.com>
2014-06-21 17:07:45 +0200

    Add CountHi

---
+++ String-2/CountHi/CountHi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountHi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountHi("abc hi ho"));
+            Console.WriteLine(CountHi("ABChi hi"));
+            Console.WriteLine(CountHi("hihi"));
+            Console.ReadLine();
+        }
+
+        /* Return the number of times that the string "hi" appears anywhere in the given string. */
+        public static int CountHi(string str)
+        {
+            var count = 0;
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                if (str.Substring(i, 2).Equals("hi"))
+                    count++;
+            }
+            return count;
+        }
+    }
+}
