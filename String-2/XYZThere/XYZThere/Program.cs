d54173da6a8dd67f2ae57c036562d2c107ca7bcf
bahagiga <baha.koyuncu@gmail.com>
2014-06-26 00:22:50 +0200

    Add XYZThere

---
+++ String-2/XYZThere/XYZThere/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZThere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(XyzThere("abcxyz"));
+            Console.WriteLine(XyzThere("abc.xyz"));
+            Console.WriteLine(XyzThere("xyz.abc"));
+            Console.ReadLine();
+        }
+
+        /* Return true if the given string contains an appearance of "xyz" where the xyz is not 
+         * directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.*/
+        public static bool XyzThere(string str)
+        {
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (i <= str.Length - 3 && str.Substring(i, 3).Equals("xyz"))
+                    if (i == 0)
+                        return true;
+                    else if (str[i - 1] != '.')
+                        return true;
+            }
+            return false;
+        }
+    }
+}
