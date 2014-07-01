7523661fec94a31ffa6e1f3d60192b7808252727
bahagiga <baha.koyuncu@gmail.com>
2014-07-01 23:40:25 +0200

    Add CountEvens

---
+++ Array-2/CountEvens/CountEvens/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CountEvens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountEvens(new[] { 2, 1, 2, 3, 4 }));
+            Console.WriteLine(CountEvens(new[] { 2, 2, 0 }));
+            Console.WriteLine(CountEvens(new[] { 1, 3, 5 }));
+            Console.ReadLine();
+        }
+
+        /* Return the number of even ints in the given array. */
+        public static int CountEvens(int[] nums)
+        {
+            var count = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] % 2 != 0) continue;
+                count++;
+            }
+            return count;
+        }
+    }
+}
