c0247ea49503e65bc1efaf3859b187452d202259
bahagiga <baha.koyuncu@gmail.com>
2014-07-03 14:24:42 +0200

    Add Lucky13

---
+++ Array-2/Lucky13/Lucky13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Lucky13(new[] { 0, 2, 4 }));
+            Console.WriteLine(Lucky13(new[] { 1, 2, 3 }));
+            Console.WriteLine(Lucky13(new[] { 1, 2, 4 }));
+            Console.ReadLine();
+        }
+
+        /* Given an array of ints, return true if the array contains no 1's and no 3's. */
+        public static bool Lucky13(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i].Equals(1) || nums[i].Equals(3))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
