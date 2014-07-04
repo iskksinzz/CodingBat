43684c5981650d01946d929f6965b1330774a5aa
bahagiga <baha.koyuncu@gmail.com>
2014-07-04 17:54:49 +0200

    Add No14

---
+++ Array-2/No14/No14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(No14(new[] { 1, 2, 3 }));
+            Console.WriteLine(No14(new[] { 1, 2, 3, 4 }));
+            Console.WriteLine(No14(new[] { 2, 3, 4 }));
+            Console.ReadLine();
+        }
+
+        /* Given an array of ints, return true if it contains no 1's or it contains no 4's. */
+        public static bool No14(int[] nums)
+        {
+            var one = false;
+            var four = false;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i].Equals(1)) { one = true; }
+                if (nums[i].Equals(4)) { four = true; }
+                if (one && four) return false;
+            }
+            return true;
+        }
+    }
+}
