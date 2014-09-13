28ad1a7eb1db92187fa859775a143985b4d291bd
bahagiga <baha.koyuncu@gmail.com>
2014-09-13 16:49:35 +0200

    Add SameEnds

---
+++ Array-2/SameEnds/SameEnds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SameEnds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SameEnd(new[] { 5, 6, 45, 99, 13, 5, 6 }, 1));
+            Console.WriteLine(SameEnd(new[] { 5, 6, 45, 99, 13, 5, 6 }, 2));
+            Console.WriteLine(SameEnd(new[] { 5, 6, 45, 99, 13, 5, 6 }, 3));
+            Console.ReadLine();
+        }
+
+        /* Return true if the group of N numbers at the start and end of the array are the same.*/
+        public static bool SameEnd(int[] nums, int len)
+        {
+            var b = true;
+            for (int i = 0; i < len; i++)
+            {
+                b = nums[i] == nums[nums.Length - len + i];
+            }
+            return b;
+        }
+    }
+}
