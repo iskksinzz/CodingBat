6a3d56428e5d541f58f1edd6a8e79f30d24e732d
bahagiga <baha.koyuncu@gmail.com>
2014-09-13 17:22:58 +0200

    Add ShiftLeft

---
+++ Array-2/ShiftLeft/ShiftLeft/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ShiftLeft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShiftLeft(new[] { 6, 2, 5, 3 }));
+            Console.WriteLine(ShiftLeft(new[] { 1, 2 }));
+            Console.WriteLine(ShiftLeft(new[] { 1 }));
+            Console.ReadLine();
+        }
+
+        /* Return an array that is "left shifted" by one */
+
+        public static int[] ShiftLeft(int[] nums)
+        {
+            var temp = nums[0];
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                nums[i] = nums[i + 1];
+            }
+            nums[nums.Length - 1] = temp;
+            return nums;
+        }
+    }
+}
