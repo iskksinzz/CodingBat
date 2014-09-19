d6891d03f5ab6857a2aca52839a2076174b2f60c
bahagiga <baha.koyuncu@gmail.com>
2014-09-19 21:16:28 +0200

    Add WithoutTen

---
+++ Array-2/WithoutTen/WithoutTen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WithoutTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WithoutTen(new[] { 1, 10, 10, 2 }));
+            Console.WriteLine(WithoutTen(new[] { 10, 2, 10 }));
+            Console.WriteLine(WithoutTen(new[] { 1, 99, 10 }));
+            Console.ReadLine();
+        }
+
+        /* Return a version of the given array where all the 10's have been removed. 
+         * The remaining elements should shift left towards the start of the array as needed, 
+         * and the empty spaces a the end of the array should be 0. */
+        public static int[] WithoutTen(int[] nums)
+        {
+            var arr = new int[nums.Length];
+            var j = 0;
+
+            foreach (int num in nums)
+            {
+                if (num != 10)
+                    arr[j++] = num;
+            }
+            return arr;
+        }
+    }
+}
