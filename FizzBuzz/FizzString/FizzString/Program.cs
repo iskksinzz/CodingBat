8ced0a3e8bdbcf83471688e806d182b4f377df8e
bahagiga <baha.koyuncu@gmail.com>
2014-09-21 18:23:13 +0200

    Add FizzString

---
+++ FizzBuzz/FizzString/FizzString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzString("fig"));
+            Console.WriteLine(FizzString("dib"));
+            Console.WriteLine(FizzString("fib"));
+            Console.ReadLine();
+        }
+
+        public static string FizzString(string str)
+        {
+            if (str.StartsWith("f") && str.EndsWith("b"))
+                return "FizzBuzz";
+            if (str.StartsWith("f"))
+                return "Fizz";
+            if (str.EndsWith("b"))
+                return "Buzz";
+
+            return str;
+        }
+    }
+}
