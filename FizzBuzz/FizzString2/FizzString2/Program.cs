6b30dbee5a1c0ba80f1b2fde27ca7db21a405e9d
bahagiga <baha.koyuncu@gmail.com>
2014-09-21 18:32:28 +0200

    Add FizzString2

---
+++ FizzBuzz/FizzString2/FizzString2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzString2(1));
+            Console.WriteLine(FizzString2(2));
+            Console.WriteLine(FizzString2(3));
+            Console.ReadLine();
+        }
+
+        public static string FizzString2(int n)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+                return "FizzBuzz!";
+
+            if (n % 3 == 0)
+                return "Fizz!";
+
+            if (n % 5 == 0)
+                return "Buzz!";
+
+            return n + "!";
+        }
+    }
+}
