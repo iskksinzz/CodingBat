a13904861ae0a6b56f91f4e2820f38aef2026692
bahagiga <baha.koyuncu@gmail.com>
2014-09-21 22:36:20 +0200

    Add FizzArray3

---
+++ FizzBuzz/FizzArray3/FizzArray3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FizzArray3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzArray3(5, 10));
+            Console.WriteLine(FizzArray3(11, 18));
+            Console.WriteLine(FizzArray3(1, 3));
+            Console.ReadLine();
+        }
+
+        public static int[] FizzArray3(int start, int end)
+        {
+            var arr = new int[end - start];
+            var j = 0;
+            for (int i = start; i < end; i++)
+            {
+                arr[j++] = i;
+            }
+            return arr;
+        }
+    }
+}
