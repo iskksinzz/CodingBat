80ac08ced1ebe102e1ec6e514eca047c4b8c8f23
bahagiga <baha.koyuncu@gmail.com>
2014-09-21 19:10:31 +0200

    Add FizzArray2

---
+++ FizzBuzz/FizzArray2/FizzArray2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzArray2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzArray2(4));
+            Console.WriteLine(FizzArray2(10));
+            Console.WriteLine(FizzArray2(2));
+            Console.ReadLine();
+        }
+
+        public static string[] FizzArray2(int n)
+        {
+            var arr = new string[n];
+            for (var i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToString(i);
+            }
+            return arr;
+        }
+    }
+}
