3eb9ace71f23bb9a6624d82e052be5377fea4ccc
bahagiga <baha.koyuncu@gmail.com>
2014-09-22 16:56:46 +0200

    Add FizzBuzz

---
+++ FizzBuzz/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(1, 6));
+            Console.WriteLine(FizzBuzz(1, 8));
+            Console.WriteLine(FizzBuzz(1, 11));
+            Console.ReadLine();
+        }
+
+        public static string[] FizzBuzz(int start, int end)
+        {
+            var arr = new string[end - start];
+            var j = 0;
+            for (int i = start; i < end; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) { arr[j++] = "FizzBuzz"; }
+                else if (i % 3 == 0) { arr[j++] = "Fizz"; }
+                else if (i % 5 == 0) { arr[j++] = "Buzz"; }
+                else { arr[j++] = Convert.ToString(i); }
+            }
+            return arr;
+        }
+    }
+}
