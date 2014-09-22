d5c84b8e916a963e7bb7995b1bf9d2b59839d2a6
bahagiga <baha.koyuncu@gmail.com>
2014-09-22 18:03:42 +0200

    Add BigDiff

---
+++ Array-2/BigDiff/BigDiff/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BigDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigDiff(new[] { 10, 3, 5, 6 }));
+            Console.WriteLine(BigDiff(new[] { 7, 2, 10, 9 }));
+            Console.WriteLine(BigDiff(new[] { 2, 10, 7, 2 }));
+            Console.ReadLine();
+        }
+
+        public static int BigDiff(int[] nums)
+        {
+            var h = nums[0];
+            var l = nums[0];
+
+            foreach (var n in nums)
+            {
+                if (n > h)
+                    h = n;
+                if (n < l)
+                    l = n;
+            }
+            return h - l;
+        }
+    }
+}
