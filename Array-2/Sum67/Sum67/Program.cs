bfff59f2494a9f3e518a0e9511ad268dbb38c082
bahagiga <baha.koyuncu@gmail.com>
2014-09-23 22:12:52 +0200

    Add Sum67

---
+++ Array-2/Sum67/Sum67/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum67(new[] { 1, 2, 2 }));
+            Console.WriteLine(Sum67(new[] { 1, 2, 2, 6, 99, 99, 7 }));
+            Console.WriteLine(Sum67(new[] { 1, 1, 6, 7, 2 }));
+            Console.ReadLine();
+        }
+
+        public static int Sum67(int[] nums)
+        {
+            var six = false;
+            var sum = 0;
+
+            foreach (int num in nums)
+            {
+                if (num == 6) { six = true; }
+                if (num == 7 && six) { six = false; continue; }
+                if (!six) { sum += num; }
+            }
+            return sum;
+        }
+    }
+}
