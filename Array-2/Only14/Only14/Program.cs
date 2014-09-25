a405103f6c78e59f0d9b4cb54bf1c05479c228af
bahagiga <baha.koyuncu@gmail.com>
2014-09-25 17:55:48 +0200

    Add Only14

---
+++ Array-2/Only14/Only14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Only14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Only14(new[] { 1, 4, 1, 4 }));
+            Console.WriteLine(Only14(new[] { 1, 4, 2, 4 }));
+            Console.WriteLine(Only14(new[] { 1, 1 }));
+            Console.ReadLine();
+        }
+
+        public static bool Only14(int[] nums)
+        {
+            foreach (var num in nums)
+            {
+                if (num != 1 && num != 4)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
