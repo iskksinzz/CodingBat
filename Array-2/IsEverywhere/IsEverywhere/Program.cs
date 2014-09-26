9e203877f274b04aa01a9408ebe9a5b54247df8b
bahagiga <baha.koyuncu@gmail.com>
2014-09-26 20:58:29 +0200

    Add IsEverywhere

---
+++ Array-2/IsEverywhere/IsEverywhere/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IsEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsEverywhere(new[] { 1, 2, 1, 3 }, 1));
+            Console.WriteLine(IsEverywhere(new[] { 1, 2, 1, 3 }, 2));
+            Console.WriteLine(IsEverywhere(new[] { 1, 2, 1, 3, 4 }, 1));
+            Console.ReadLine();
+        }
+
+        public static bool IsEverywhere(int[] nums, int val)
+        {
+            for (var i = 0; i <= nums.Length - 2; i++)
+            {
+                if (nums[i] != val && nums[i + 1] != val)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
