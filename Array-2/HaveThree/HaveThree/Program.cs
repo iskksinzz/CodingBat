cc1086d49ef820154f88b5abf514eae89f67513a
bahagiga <baha.koyuncu@gmail.com>
2014-09-28 11:29:39 +0200

    Add HaveThree

---
+++ Array-2/HaveThree/HaveThree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HaveThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasThree(new[] { 3, 1, 3, 1, 3 }));
+            Console.WriteLine(HasThree(new[] { 3, 1, 3, 3 }));
+            Console.WriteLine(HasThree(new[] { 3, 4, 3, 3, 4 }));
+            Console.ReadLine();
+        }
+
+        public static bool HasThree(int[] nums)
+        {
+            var c = 0;
+            for (var i = 0; i < nums.Length - 2; i++)
+            {
+                if (nums[i] == 3 && nums[i + 2] == 3)
+                    c++;
+            }
+            return c == 2;
+        }
+    }
+}
