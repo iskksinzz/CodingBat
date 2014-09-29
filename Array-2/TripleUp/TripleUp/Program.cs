5aa635782c77d847d2ec922c40d58d43c44c451d
bahagiga <baha.koyuncu@gmail.com>
2014-09-29 18:24:36 +0200

    Add TripleUp

---
+++ Array-2/TripleUp/TripleUp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TripleUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TripleUp(new[] { 1, 4, 5, 6, 2 }));
+            Console.WriteLine(TripleUp(new[] { 1, 2, 3 }));
+            Console.WriteLine(TripleUp(new[] { 1, 2, 4 }));
+            Console.ReadLine();
+        }
+
+        public static bool TripleUp(int[] nums)
+        {
+            for (var i = 0; i <= nums.Length - 3; i++)
+            {
+                if ((nums[i] == nums[i + 1] - 1) && (nums[i + 1] + 1 == nums[i + 2]))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
