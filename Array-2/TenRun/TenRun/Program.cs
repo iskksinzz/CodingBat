788809d6ee07e2791ce9d40ff30eb0f69473be76
bahagiga <baha.koyuncu@gmail.com>
2014-09-30 19:01:17 +0200

    Add TenRun

---
+++ Array-2/TenRun/TenRun/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TenRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TenRun(new[] { 2, 10, 3, 4, 20, 5 }));
+            Console.WriteLine(TenRun(new[] { 10, 1, 20, 2 }));
+            Console.WriteLine(TenRun(new[] { 10, 1, 9, 20 }));
+            Console.ReadLine();
+        }
+
+        public static int[] TenRun(int[] nums)
+        {
+            var x = nums[0];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (x % 10 != 0)
+                    x = nums[i];
+                if (nums[i] % 10 == 0)
+                    x = nums[i];
+                nums[i] = x;
+            }
+            return nums;
+        }
+    }
+}
