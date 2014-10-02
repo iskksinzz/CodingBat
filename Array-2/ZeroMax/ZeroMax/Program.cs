502e7df814c737be4fa2d734f0893fca6c3ee254
bahagiga <baha.koyuncu@gmail.com>
2014-10-02 19:44:59 +0200

    Add ZeroMax

---
+++ Array-2/ZeroMax/ZeroMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ZeroMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ZeroMax(new[] { 0, 5, 0, 3 }));
+            Console.WriteLine(ZeroMax(new[] { 0, 4, 0, 3 }));
+            Console.WriteLine(ZeroMax(new[] { 0, 1, 0 }));
+            Console.ReadLine();
+        }
+
+        public static int[] ZeroMax(int[] nums)
+        {
+            var t = 0;
+            for (var i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] % 2 == 1 && nums[i] > t)
+                    t = nums[i];
+                if (nums[i] != 0) continue;
+                nums[i] = t;
+            }
+            return nums;
+        }
+    }
+}
