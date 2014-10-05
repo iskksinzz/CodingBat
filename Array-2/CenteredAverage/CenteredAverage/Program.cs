8f15ec8941bdb8640e27ca0e81956057abefe29d
bahagiga <baha.koyuncu@gmail.com>
2014-10-05 18:53:10 +0200

    Add CenteredAverage

---
+++ Array-2/CenteredAverage/CenteredAverage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CenteredAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CenteredAverage(new[] { 1, 2, 3, 4, 100 }));
+            Console.ReadLine();
+        }
+
+        public static int CenteredAverage(int[] nums)
+        {
+            var max = nums[0];
+            var min = nums[0];
+            var c = 0;
+
+            foreach (var i in nums)
+            {
+                if (i > max) max = i;
+                if (i < min) min = i;
+                c += i;
+            }
+            return (c - (max + min)) / (nums.Length - 2);
+        }
+    }
+}
