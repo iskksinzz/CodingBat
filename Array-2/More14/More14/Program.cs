caed93f13d4ee25b8c56c2a5702940c6fceb0b2c
bahagiga <baha.koyuncu@gmail.com>
2014-10-07 18:11:29 +0200

    Add More14

---
+++ Array-2/More14/More14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace More14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(More14(new[] { 1, 4, 1 }));
+            Console.WriteLine(More14(new[] { 1, 4, 1, 4 }));
+            Console.WriteLine(More14(new[] { 1, 1 }));
+            Console.ReadLine();
+        }
+
+        public static bool More14(int[] nums)
+        {
+            var o = 0;
+            var f = 0;
+            foreach (var i in nums)
+            {
+                if (i == 1) o++;
+                if (i == 4) f++;
+            }
+            return o > f;
+        }
+    }
+}
