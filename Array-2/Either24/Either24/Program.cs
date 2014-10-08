14025b944352407d3383be3e37878f9b3c61831c
bahagiga <baha.koyuncu@gmail.com>
2014-10-08 22:17:00 +0200

    Add Either24

---
+++ Array-2/Either24/Either24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Either24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Either24(new[] { 1, 2, 2 }));
+            Console.WriteLine(Either24(new[] { 4, 4, 1 }));
+            Console.WriteLine(Either24(new[] { 4, 4, 1, 2, 2 }));
+            Console.ReadLine();
+        }
+
+        public static bool Either24(int[] nums)
+        {
+            var two = false;
+            var four = false;
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == 2 && nums[i + 1] == 2) two = true;
+                if (nums[i] == 4 && nums[i + 1] == 4) four = true;
+            }
+            return (!two || !four) && (four || two);
+        }
+    }
+}
