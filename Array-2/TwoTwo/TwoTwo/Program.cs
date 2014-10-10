46b6ee60d87f787b31fbbe0f7e490fcc31234946
bahagiga <baha.koyuncu@gmail.com>
2014-10-10 19:22:19 +0200

    Add TwoTwo

---
+++ Array-2/TwoTwo/TwoTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TwoTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoTwo(new[] { 4, 2, 2, 3 }));
+            Console.WriteLine(TwoTwo(new[] { 2, 2, 4, 2 }));
+            Console.WriteLine(TwoTwo(new[] { 4, 2, 2, 2 }));
+            Console.ReadLine();
+        }
+
+        public static bool TwoTwo(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 2) continue;
+                if (i >= 1 && nums[i - 1] == 2) continue;
+                if (i < (nums.Length - 1) && nums[i + 1] == 2) continue;
+                return false;
+            }
+            return true;
+        }
+    }
+}
