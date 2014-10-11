51c6b3bbd2fadafc570f90a34e87dfcc09acc780
bahagiga <baha.koyuncu@gmail.com>
2014-10-11 14:35:42 +0200

    Add Pre4

---
+++ Array-2/Pre4/Pre4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pre4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pre4(new[] { 1, 2, 4, 1 }));
+            Console.ReadLine();
+        }
+
+        public static int[] Pre4(int[] nums)
+        {
+            var k = 0;
+            while (nums[k] != 4) { k++; }
+
+            var arr = new int[k];
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = nums[i];
+            return arr;
+        }
+    }
+}
