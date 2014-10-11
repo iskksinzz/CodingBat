ad9c957f231667bfea38a8259a928e44a16027fa
bahagiga <baha.koyuncu@gmail.com>
2014-10-11 17:21:52 +0200

    Add ZeroFront

---
+++ Array-2/ZeroFront/ZeroFront/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ZeroFront
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(ZeroFront(new[] { 1, 0, 0, 1 }));
+            Console.WriteLine(ZeroFront(new[] { 0, 1, 1, 0, 1 }));
+            Console.WriteLine(ZeroFront(new[] { 1, 0 }));
+            Console.ReadLine();
+        }
+
+        public static int[] ZeroFront(int[] nums)
+        {
+            var j = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0) continue;
+                nums[i] = nums[j];
+                nums[j] = 0;
+                j++;
+            }
+            return nums;
+        }
+    }
+}
