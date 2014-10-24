80a0ed3e91be686c006c8d443374900b779968fb
bahagiga <baha.koyuncu@gmail.com>
2014-10-24 22:13:05 +0200

    Add MakeBricks

---
+++ Logic-2/MakeBricks/MakeBricks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MakeBricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeBricks(3, 1, 8));
+            Console.WriteLine(MakeBricks(3, 1, 9));
+            Console.WriteLine(MakeBricks(3, 2, 10));
+            Console.ReadLine();
+        }
+
+        public static bool MakeBricks(int small, int big, int goal)
+        {
+            return !(goal > big * 5 + small) && !(goal % 5 > small);
+        }
+    }
+}
