a3168ddec60571ac60ef155061ef52964c2c6432
bahagiga <baha.koyuncu@gmail.com>
2014-10-26 21:50:52 +0100

    Add Blackjack

---
+++ Logic-2/Blackjack/Blackjack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Blackjack(19, 21));
+            Console.WriteLine(Blackjack(21, 19));
+            Console.WriteLine(Blackjack(19, 22));
+            Console.ReadLine();
+        }
+
+        public static int Blackjack(int a, int b)
+        {
+            return a > 21 && b > 21 ? 0 : ((21 - a < 21 - b) && (a < 22) ? a : (b < 22 ? b : a));
+        }
+    }
+}
