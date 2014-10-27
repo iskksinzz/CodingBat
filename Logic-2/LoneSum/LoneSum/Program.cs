55227707d25b43d6fa751f78bf4325d94a23a6a4
bahagiga <baha.koyuncu@gmail.com>
2014-10-27 22:05:00 +0100

    Add LoneSum

---
+++ Logic-2/LoneSum/LoneSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LoneSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LoneSum(1, 2, 3));
+            Console.WriteLine(LoneSum(3, 2, 3));
+            Console.WriteLine(LoneSum(3, 3, 3));
+            Console.ReadLine();
+        }
+
+        public static int LoneSum(int a, int b, int c)
+        {
+            return a == b && b == c ? 0 : (a == b ? c : (b == c ? a : (a == c ? b : a + b + c)));
+        }
+    }
+}
