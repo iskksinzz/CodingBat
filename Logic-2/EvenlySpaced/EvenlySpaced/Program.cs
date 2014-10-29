d93da1a0fa920baf1c92dc6bb037ac57a57d0348
bahagiga <baha.koyuncu@gmail.com>
2014-10-29 20:21:01 +0100

    Add EvenlySpaced

---
+++ Logic-2/EvenlySpaced/EvenlySpaced/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenlySpaced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenlySpaced(2, 4, 6));
+            Console.WriteLine(EvenlySpaced(4, 6, 2));
+            Console.WriteLine(EvenlySpaced(4, 6, 3));
+            Console.ReadLine();
+        }
+
+        public static bool EvenlySpaced(int a, int b, int c)
+        {
+            return a - b == b - c || a - c == b - a || b - c == a - b || a - c == c - b;
+        }
+    }
+}
