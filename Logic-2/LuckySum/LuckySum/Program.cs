8d90ed8cf6cd8925f08ec0b8a8a0afa5471e8d06
bahagiga <baha.koyuncu@gmail.com>
2014-10-30 20:25:00 +0100

    Add LuckySum

---
+++ Logic-2/LuckySum/LuckySum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LuckySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LuckySum(1, 2, 3));
+            Console.WriteLine(LuckySum(1, 2, 13));
+            Console.WriteLine(LuckySum(1, 13, 3));
+            Console.ReadLine();
+        }
+
+        public static int LuckySum(int a, int b, int c)
+        {
+            return a == 13 ? 0 : b == 13 ? a : c == 13 ? a + b : a + b + c;
+        }
+    }
+}
