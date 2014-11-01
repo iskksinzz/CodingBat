2b9b261c4c69450f59b2104a225b8ed776850d45
bahagiga <baha.koyuncu@gmail.com>
2014-11-01 18:59:11 +0100

    Add CloseFar

---
+++ Logic-2/CloseFar/CloseFar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CloseFar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CloseFar(1, 2, 10));
+            Console.WriteLine(CloseFar(1, 2, 3));
+            Console.WriteLine(CloseFar(4, 1, 3));
+            Console.ReadLine();
+        }
+
+        public static bool CloseFar(int a, int b, int c)
+        {
+            return Math.Abs(b - c) > 1 && (Math.Abs(a - b) < 2 && Math.Abs(a - c) > 1 || Math.Abs(a - c) < 2 && Math.Abs(a - b) > 1);
+        }
+    }
+}
