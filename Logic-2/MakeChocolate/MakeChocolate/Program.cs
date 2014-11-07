f9d14d6ac942cb3423dbe31e78949d37c92f22b6
bahagiga <baha.koyuncu@gmail.com>
2014-11-07 22:27:34 +0100

    Add MakeChocolate

---
+++ Logic-2/MakeChocolate/MakeChocolate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MakeChocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeChocolate(4, 1, 9));
+            Console.WriteLine(MakeChocolate(4, 1, 10));
+            Console.WriteLine(MakeChocolate(4, 1, 7));
+            Console.ReadLine();
+        }
+
+        public static int MakeChocolate(int small, int big, int goal)
+        {
+            var n = goal - Math.Min(big, goal / 5) * 5;
+            return (n <= small) ? n : -1;
+        }
+    }
+}
+
