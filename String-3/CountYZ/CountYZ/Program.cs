4241ce6ed09e0740b8bcf4fc57e3feef74ce7d7c
bahagiga <baha.koyuncu@gmail.com>
2014-11-08 22:03:17 +0100

    Add countYZ

---
+++ String-3/CountYZ/CountYZ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CountYZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountYZ("fez day"));
+            Console.WriteLine(CountYZ("day fez"));
+            Console.WriteLine(CountYZ("day fyyyz"));
+            Console.ReadLine();
+        }
+
+        public static int CountYZ(string str)
+        {
+            var c = 0;
+            str = str.ToLower() + " ";
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                if ((str[i] == 'y' || str[i] == 'z') && !char.IsLetter(str[i + 1]))
+                    c++;
+            }
+            return c;
+        }
+    }
+}
