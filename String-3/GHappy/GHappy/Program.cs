8a1ef9d53b7c40e3be57003cf82a8565a3c0fc00
bahagiga <baha.koyuncu@gmail.com>
2014-11-15 21:16:26 +0100

    Add GHappy

---
+++ String-3/GHappy/GHappy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GHappy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GHappy("xxggxx"));
+            Console.WriteLine(GHappy("xxgxx"));
+            Console.WriteLine(GHappy("xxggyygxx"));
+            Console.ReadLine();
+        }
+
+        public static bool GHappy(string str)
+        {
+            var result = false;
+            if (str == string.Empty) { return true; }
+            for (var i = 1; i <= str.Length - 1; i++)
+            {
+                if (str[i] != 'g') continue;
+                result = str[i - 1] == 'g' || i < str.Length - 1 && str[i + 1] == 'g';
+            }
+            return result;
+        }
+    }
+}
