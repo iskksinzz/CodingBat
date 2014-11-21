45bebf15021dc9d28e9f48d0445e9464e9ccdd1a
bahagiga <baha.koyuncu@gmail.com>
2014-11-21 20:09:56 +0100

    Add SameEnds

---
+++ String-3/SameEnds/SameEnds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SameEnds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SameEnds("abXYab"));
+            Console.WriteLine(SameEnds("xx"));
+            Console.WriteLine(SameEnds("xxx"));
+            Console.ReadLine();
+        }
+
+        public static string SameEnds(string str)
+        {
+            var s = "";
+            for (var i = 1; i < str.Length/2+1; i++)
+            {
+                if (str.Substring(0, i).Equals(str.Substring(str.Length - i)))
+                    s = str.Substring(0, i);
+            }
+            return s;
+        }
+    }
+}
