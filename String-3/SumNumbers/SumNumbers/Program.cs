98453b923e70bb9d05d84a36924a07bddf288b9b
bahagiga <baha.koyuncu@gmail.com>
2014-11-22 11:28:39 +0100

    Add SumNumbers

---
+++ String-3/SumNumbers/SumNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(SumNumbers("abc123xyz"));
+            Console.WriteLine(SumNumbers("aa11b33"));
+            Console.WriteLine(SumNumbers("7 11"));
+            Console.WriteLine(SumNumbers("Chocolate"));
+            Console.WriteLine(SumNumbers("5hoco1a1e"));
+            Console.WriteLine(SumNumbers("5$$1;;1!!"));
+            Console.WriteLine(SumNumbers("a1234bb11"));
+            Console.WriteLine(SumNumbers(""));
+            Console.WriteLine(SumNumbers("a22bbb3"));
+            Console.ReadLine();
+        }
+
+        public static int SumNumbers(string str)
+        {
+            var sum = 0;
+            foreach (var number in Regex.Split(str, "\\D+"))
+            {
+                int n;
+                var isNumeric = int.TryParse(number, out n);
+                if (isNumeric)
+                    sum += n;
+            }
+            return sum;
+        }
+    }
+}
