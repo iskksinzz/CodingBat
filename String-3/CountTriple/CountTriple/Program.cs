b04e0ff8c08a9683a682e4ddee2fe6a41f44cdf9
bahagiga <baha.koyuncu@gmail.com>
2014-12-14 00:45:05 +0100

    Add CountTriple

---
+++ String-3/CountTriple/CountTriple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountTriple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountTriple("abcXXXabc"));
+            Console.WriteLine(CountTriple("xxxabyyyycd"));
+            Console.WriteLine(CountTriple("a"));
+            Console.ReadLine();
+        }
+
+        private static int CountTriple(string str)
+        {
+            var c = 0;
+            for (var i = 0; i < str.Length - 2; i++)
+                if (str[i].Equals(str[i + 1]) && str[i].Equals(str[i + 2]))
+                    c++;
+            return c;
+        }
+    }
+}
