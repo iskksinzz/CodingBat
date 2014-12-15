ed5cbf9df4e68db84807015bc52117716a1d02df
bahagiga <baha.koyuncu@gmail.com>
2014-12-15 13:04:03 +0100

    Add MirrorEnds

---
+++ String-3/MirrorEnds/MirrorEnds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MirrorEnds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MirrorEnds("abXYZba"));
+            Console.WriteLine(MirrorEnds("abca"));
+            Console.WriteLine(MirrorEnds("aba"));
+            Console.ReadLine();
+        }
+
+        private static string MirrorEnds(string str)
+        {
+            var s = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (!str[i].Equals(str[str.Length - i - 1]))
+                    break;
+                s += str[i];
+            }
+            return s;
+        }
+    }
+}
