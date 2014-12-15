58d3181be16da9ee9736fa2416e33fdac1327a29
bahagiga <baha.koyuncu@gmail.com>
2014-12-15 20:50:35 +0100

    Add NotReplace

---
+++ String-3/NotReplace/NotReplace/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace NotReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NotReplace("is test"));
+            Console.WriteLine(NotReplace("is-is"));
+            Console.WriteLine(NotReplace("This is right"));
+            Console.ReadLine();
+        }
+
+        private static string NotReplace(string str)
+        {
+            return Regex.Replace(str, "\\w*(?<![A-Za-z])is\\b", "is not");
+        }
+    }
+}
