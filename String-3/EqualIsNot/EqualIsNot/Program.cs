d913fd257577a9c30f6062e1feeebd8ed42845f4
bahagiga <baha.koyuncu@gmail.com>
2014-12-22 00:06:57 +0100

    Add EqualIsNot

---
+++ String-3/EqualIsNot/EqualIsNot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EqualIsNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EqualIsNot("This is not"));
+            Console.WriteLine(EqualIsNot("This is notnot"));
+            Console.WriteLine(EqualIsNot("noisxxnotyynotxisi"));
+            Console.ReadLine();
+        }
+
+        private static bool EqualIsNot(string str)
+        {
+            return Regex.Matches(str, "is").Count == Regex.Matches(str, "not").Count;
+        }
+    }
+}
