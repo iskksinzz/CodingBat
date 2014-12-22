7524c0e90a5a54c3b3d85ac88818c9d31b2a27af
bahagiga <baha.koyuncu@gmail.com>
2014-12-22 16:54:06 +0100

    Add SumDigits

---
+++ String-3/SumDigits/SumDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigits("aa1bc2d3"));
+            Console.WriteLine(SumDigits("aa11b33"));
+            Console.WriteLine(SumDigits("Chocolate"));
+            Console.ReadLine();
+        }
+
+        private static int SumDigits(string str)
+        {
+            return str.Where(char.IsDigit).Sum(t => int.Parse(t.ToString()));
+        }
+    }
+}
