fdd49fb7b4a2ccdd0e3fcb24f81f1fbba8056458
bahagiga <baha.koyuncu@gmail.com>
2014-12-22 18:35:43 +0100

    Add MaxBlock

---
+++ String-3/MaxBlock/MaxBlock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MaxBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxBlock("hoopla"));
+            Console.WriteLine(MaxBlock("abbCCCddBBBxx"));
+            Console.WriteLine(MaxBlock(""));
+            Console.ReadLine();
+        }
+
+        private static int MaxBlock(string str)
+        {
+            var max = 0;
+            for (var i = 0; i < str.Length; i++)
+            {
+                var c = 0;
+                for (var j = i; j < str.Length; j++)
+                {
+                    if (str[i].Equals(str[j])) c++;
+                    else break;
+                }
+                if (c > max) max = c;
+            }
+            return max;
+        }
+    }
+}
