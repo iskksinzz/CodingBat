89aefe7b0a3ecd53e640264327c18c316d1afdd6
bahagiga <baha.koyuncu@gmail.com>
2014-12-23 23:46:59 +0100

    Add CanBalance

---
+++ Array-3/CanBalance/CanBalance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace CanBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanBalance(new[] { 1, 1, 1, 2, 1 }));
+            Console.WriteLine(CanBalance(new[] { 2, 1, 1, 2, 1 }));
+            Console.WriteLine(CanBalance(new[] { 10, 10 }));
+            Console.ReadLine();
+        }
+
+        private static bool CanBalance(int[] nums)
+        {
+            var val = nums.Sum();
+            if (val % 2 == 0)
+            {
+                var j = 0;
+                foreach (var i in nums)
+                {
+                    j += i;
+                    if (j == val / 2) { return true; }
+                }
+            }
+            return false;
+
+        }
+    }
+}
