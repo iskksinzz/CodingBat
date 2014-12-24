e2bf97f08b0847feba58d3758deaddea5f066d51
bahagiga <baha.koyuncu@gmail.com>
2014-12-24 13:58:42 +0100

    Add SeriesUp

---
+++ Array-3/SeriesUp/SeriesUp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SeriesUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SeriesUp(3));
+            Console.WriteLine(SeriesUp(4));
+            Console.WriteLine(SeriesUp(2));
+            Console.ReadLine();
+        }
+
+        private static int[] SeriesUp(int n)
+        {
+            var arr = new int[n * (n + 1) / 2];
+            var c = 0;
+            for (var i = 0; i <= n; i++)
+            {
+                for (var j = 1; j < i + 1; j++)
+                    arr[c++] = j;
+            }
+            return arr;
+        }
+    }
+}
