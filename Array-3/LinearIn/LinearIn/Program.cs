72e6f7b5356c37be97e250ea9fee913504e4ae17
bahagiga <baha.koyuncu@gmail.com>
2015-01-09 00:25:27 +0100

    Add LinearIn

---
+++ Array-3/LinearIn/LinearIn/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LinearIn(new[] { 1, 2, 4, 6 }, new[] { 2, 4 }));
+            Console.WriteLine(LinearIn(new[] { 1, 2, 4, 6 }, new[] { 2, 3, 4 }));
+            Console.WriteLine(LinearIn(new[] { 1, 2, 4, 4, 6 }, new[] { 2, 4 }));
+            Console.ReadLine();
+        }
+
+        static bool LinearIn(int[] outer, int[] inner)
+        {
+            if (inner.Length == 0) return true;
+
+            var j = 0;
+            foreach (var i in outer)
+            {
+                if (inner[j] == i) { j++; }
+                if (j == inner.Length) { return true; }
+            }
+            return false;
+        }
+    }
+}
