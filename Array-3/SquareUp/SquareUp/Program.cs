de01c7c43d2cd21402a9b63ccbe05c7762f16a87
bahagiga <baha.koyuncu@gmail.com>
2015-02-19 21:52:28 +0100

    Add SquareUp

---
+++ Array-3/SquareUp/SquareUp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SquareUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SquareUp(3);
+            Console.ReadLine();
+        }
+        public static int[] SquareUp(int n)
+        {
+            var arr = new int[n * n];
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (n - j > i + 1)
+                    {
+                        arr[i * n + j] = 0;
+                    }
+                    else
+                    {
+                        arr[i * n + j] = n - j;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
