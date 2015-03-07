96e799852328073817647a532bd7578db53fa012
bahagiga <baha.koyuncu@gmail.com>
2015-03-07 22:47:51 +0100

    Add Factorial

---
+++ Recursion-1/Factorial/Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Factorial(3));
+            Console.ReadLine();
+        }
+
+        /* Given n of 1 or more, return the factorial of n.
+         * Compute the result recursively (without loops). */
+        public static int Factorial(int n)
+        {
+            return n == 1 ? n : n * Factorial(n - 1);
+        }
+    }
+}
