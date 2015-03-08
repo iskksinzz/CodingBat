01e07f5d49007766701d55b286403bd548d1e200
bahagiga <baha.koyuncu@gmail.com>
2015-03-08 18:10:37 +0100

    Add BunnyEars2

---
+++ Recursion-1/BunnyEars2/BunnyEars2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BunnyEars2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(BunnyEars2(2));
+            Console.ReadLine();
+        }
+
+        public static int BunnyEars2(int bunnies)
+        {
+            return bunnies == 0 ? 0 : bunnies % 2 == 0 ? 3 + BunnyEars2(bunnies - 1) : 2 + BunnyEars2(bunnies - 1);
+        }
+    }
+}
