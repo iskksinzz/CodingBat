fed7b9e0559501f5177227ae011079c6c31e9d74
bahagiga <baha.koyuncu@gmail.com>
2015-03-09 20:32:03 +0100

    Add BunnyEars

---
+++ Recursion-1/BunnyEars/BunnyEars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BunnyEars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BunnyEars(2));
+            Console.ReadLine();
+        }
+
+        public static int BunnyEars(int bunnies)
+        {
+            return bunnies * 2;
+        }
+
+    }
+}
