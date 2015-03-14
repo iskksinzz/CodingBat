0debcaba2db609e54fef79ba13d155496d393208
bahagiga <baha.koyuncu@gmail.com>
2015-03-14 20:06:19 +0100

    Add Fibonacci

---
+++ Recursion-1/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(2));
+            Console.ReadLine();
+        }
+
+        public static int Fibonacci(int n)
+        {
+            return n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
