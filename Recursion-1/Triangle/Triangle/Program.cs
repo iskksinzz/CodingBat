3c5dbfe2207b32039d263453489009f780d83fba
bahagiga <baha.koyuncu@gmail.com>
2015-03-15 14:47:04 +0100

    Add Triangle

---
+++ Recursion-1/Triangle/Triangle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Triangle(4));
+            Console.ReadLine();
+        }
+
+        public static int Triangle(int rows)
+        {
+            return rows == 0 ? rows : Triangle(rows - 1) + rows;
+        }
+    }
+}
