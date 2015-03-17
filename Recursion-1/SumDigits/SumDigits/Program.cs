89c81985eaa7b075a0793f72de40a559b34ccbca
bahagiga <baha.koyuncu@gmail.com>
2015-03-17 19:05:00 +0100

    Add SumDigits

---
+++ Recursion-1/SumDigits/SumDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigits(126));
+            Console.ReadLine();
+        }
+
+        public static int SumDigits(int n)
+        {
+            return n == 0 ? 0 : SumDigits(n / 10) + n % 10;
+        }
+    }
+}
