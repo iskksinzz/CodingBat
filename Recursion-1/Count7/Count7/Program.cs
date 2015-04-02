0ddf0f21e03bdb07d17a910baaaaa42ddedf21e9
Baha <b@b0x.dk>
2015-04-03 00:08:49 +0200

    Add Count7

---
+++ Recursion-1/Count7/Count7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Count7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Count7(717));
+            Console.ReadLine();
+        }
+
+        public static int Count7(int n)
+        {
+            return n == 0 ? 0 : n % 10 == 7 ? 1 + Count7(n / 10) : Count7(n / 10);
+        }
+    }
+}
