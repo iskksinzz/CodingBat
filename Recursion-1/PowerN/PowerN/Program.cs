bc5a700525a67d7efeb4299700a68c5eef279e1b
Baha <baha.koyuncu@gmail.com>
2015-04-09 22:47:44 +0200

    Add PowerN

---
+++ Recursion-1/PowerN/PowerN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PowerN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerN(3, 3));
+            Console.ReadLine();
+        }
+
+        private static int PowerN(int n, int exponent)
+        {
+            return exponent == 1 ? n : n * PowerN(n, exponent - 1);
+        }
+    }
+}
