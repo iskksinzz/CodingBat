aae2857e3684ed385f4bc00baf3d44616b78489a
Baha <baha.koyuncu@gmail.com>
2015-04-10 20:25:31 +0200

    Add CountX

---
+++ Recursion-1/CountX/CountX/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CountX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountX("xxhixx"));
+            Console.WriteLine(CountX("xhixhix"));
+            Console.WriteLine(CountX("hi"));
+            Console.ReadLine();
+        }
+
+        private static int CountX(string str)
+        {
+            return str.Length == 0 ? 0 : str[0] == 'x' ? 1 + CountX(str.Substring(1)) : CountX(str.Substring(1));
+        }
+    }
+}
