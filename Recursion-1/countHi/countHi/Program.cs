e36d6289f2ed14091ce6eda884f86411bf604d6e
Baha <baha.koyuncu@gmail.com>
2015-04-10 21:05:23 +0200

    Add CountHi

---
+++ Recursion-1/countHi/countHi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace countHi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountHi("xxhixx"));
+            Console.WriteLine(CountHi("xhixhix"));
+            Console.WriteLine(CountHi("hi"));
+            Console.ReadLine();
+        }
+
+        private static int CountHi(String str)
+        {
+            if (str.Length <= 1)
+                return 0;
+
+            return str.Substring(0, 2).Equals("hi") ? 1 + CountHi(str.Substring(1)) : CountHi(str.Substring(1));
+        }
+    }
+}
