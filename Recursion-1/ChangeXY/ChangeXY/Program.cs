3a021c7d635adc9ec9fdde917b95199bab4d0e62
Baha <baha.koyuncu@gmail.com>
2015-04-11 23:59:27 +0200

    Add ChangeXY

---
+++ Recursion-1/ChangeXY/ChangeXY/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChangeXY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChangeXY("codex"));
+            Console.WriteLine(ChangeXY("xxhixx"));
+            Console.WriteLine(ChangeXY("xhixhix"));
+            Console.ReadLine();
+        }
+
+        private static string ChangeXY(string str)
+        {
+            if (str.Length < 1)
+                return str;
+            var front = str[0] == 'x' ? 'y' : str[0];
+            return front + ChangeXY(str.Substring(1));
+        }
+    }
+}
