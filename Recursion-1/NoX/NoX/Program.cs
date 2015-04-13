b811d6ca6c8a7deb1fe1358b497f9b6aaa043ccb
Baha <baha.koyuncu@gmail.com>
2015-04-13 19:57:57 +0200

    Add Nox

---
+++ Recursion-1/NoX/NoX/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NoX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NoX("xaxb"));
+            Console.WriteLine(NoX("abc"));
+            Console.WriteLine(NoX("xx"));
+            Console.ReadLine();
+        }
+
+        private static string NoX(string str)
+        {
+            return str.Length < 1
+                ? ""
+                : str[0].Equals('x') ? "" + NoX(str.Substring(1)) : str[0] + NoX(str.Substring(1));
+        }
+    }
+}
