36ceccdd3dd13cb4378da0873c109a025ef7800a
Baha <baha.koyuncu@gmail.com>
2015-04-14 19:13:11 +0200

    Add Array6

---
+++ Recursion-1/Array6/Array6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Array6(new[] { 1, 6, 4 }, 0));
+            Console.WriteLine(Array6(new[] { 1, 4 }, 0));
+            Console.WriteLine(Array6(new[] { 6 }, 0));
+            Console.ReadLine();
+        }
+
+        private static bool Array6(int[] nums, int index)
+        {
+            if (nums.Length == 0)
+                return false;
+            if (nums[index] == 6)
+                return true;
+            return nums.Length > index + 1 && Array6(nums, index + 1);
+        }
+    }
+}
