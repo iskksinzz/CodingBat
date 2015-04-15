2425608f536ca2271bc4047a3935a1b319e15538
Baha <baha.koyuncu@gmail.com>
2015-04-15 20:23:38 +0200

    Add Array11

---
+++ Recursion-1/Array11/Array11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Array11(new[] { 1, 2, 11 }, 0));
+            Console.WriteLine(Array11(new[] { 11, 11 }, 0));
+            Console.WriteLine(Array11(new[] { 1, 2, 3, 4 }, 0));
+            Console.ReadLine();
+        }
+
+        private static int Array11(int[] nums, int index)
+        {
+            if (nums.Length < index + 1) return 0;
+            return nums[index] == 11 ? 1 + Array11(nums, index + 1) : Array11(nums, index + 1);
+        }
+    }
+}
