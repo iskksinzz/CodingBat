02be22402efe07e61d23f6912b51961d3154aef8
Baha <baha.koyuncu@gmail.com>
2015-04-17 20:19:07 +0200

    Add AllStar

---
+++ Recursion-1/AllStar/AllStar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AllStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AllStar("hello"));
+            Console.WriteLine(AllStar("abc"));
+            Console.WriteLine(AllStar("ab"));
+            Console.ReadLine();
+        }
+
+        private static string AllStar(string str)
+        {
+            if (str.Length <= 1)
+                return str;
+            return str[0] + "*" + AllStar(str.Substring(1));
+        }
+    }
+}
