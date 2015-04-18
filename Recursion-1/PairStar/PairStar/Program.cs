06301a312eaa451a8579361ce7b11786a60c66a7
Baha <baha.koyuncu@gmail.com>
2015-04-18 11:50:35 +0200

    Add PairStar

---
+++ Recursion-1/PairStar/PairStar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PairStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PairStar("hello"));
+            Console.WriteLine(PairStar("xxyy"));
+            Console.WriteLine(PairStar("aaaa"));
+            Console.ReadLine();
+        }
+
+        private static string PairStar(string str)
+        {
+            if (str.Length < 2)
+                return str;
+            if (str[0].Equals(str[1]))
+                return str[0] + "*" + PairStar(str.Substring(1));
+            return str[0] + PairStar(str.Substring(1));
+        }
+    }
+}
