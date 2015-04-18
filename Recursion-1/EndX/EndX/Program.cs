f1a741ac0dcf7fdad3b0b18cd8a98b9061ab1d16
Baha <baha.koyuncu@gmail.com>
2015-04-18 23:04:34 +0200

    Add EndX

---
+++ Recursion-1/EndX/EndX/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EndX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EndX("xxre"));
+            Console.WriteLine(EndX("xxhixx"));
+            Console.WriteLine(EndX("xhixhix"));
+            Console.ReadLine();
+        }
+
+        private static string EndX(string str)
+        {
+            if (str.Length < 2)
+                return str;
+            if (str[0].Equals('x'))
+                return EndX(str.Substring(1)) + str[0];
+            return str[0] + EndX(str.Substring(1));
+        }
+    }
+}
