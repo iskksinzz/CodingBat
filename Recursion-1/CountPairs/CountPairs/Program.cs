4068201d3d2f9e16c3dafdc2a0a6115d0963a7b9
Baha <baha.koyuncu@gmail.com>
2015-04-20 18:28:17 +0200

    Add CountPairs

---
+++ Recursion-1/CountPairs/CountPairs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CountPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountPairs("axa"));
+            Console.WriteLine(CountPairs("axax"));
+            Console.WriteLine(CountPairs("axbx"));
+            Console.ReadLine();
+        }
+
+        private static int CountPairs(string str)
+        {
+            if (str.Length < 3)
+                return 0;
+            if (str[0].Equals(str[2]))
+                return 1 + CountPairs(str.Substring(1));
+
+            return CountPairs(str.Substring(1));
+        }
+    }
+}
