a270e7f4a7303ea54885b7b5b8e9e59a372779df
Baha <baha.koyuncu@gmail.com>
2015-04-21 19:52:25 +0200

    Add CountAbc

---
+++ Recursion-1/CountAbc/CountAbc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountAbc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountAbc("abc"));
+            Console.WriteLine(CountAbc("abcxxabc"));
+            Console.WriteLine(CountAbc("abaxxaba"));
+            Console.ReadLine();
+        }
+
+        private static int CountAbc(string str)
+        {
+            if (str.Length < 3)
+                return 0;
+            if (str.Substring(0, 3).Equals("abc") || str.Substring(0, 3).Equals("aba"))
+                return 1 + CountAbc(str.Substring(1));
+            return CountAbc(str.Substring(1));
+        }
+    }
+}
