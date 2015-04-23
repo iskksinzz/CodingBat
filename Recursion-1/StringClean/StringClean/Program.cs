dc45c583fa4c8d1de7751cf8d7b9db9f78d5d208
Baha <baha.koyuncu@gmail.com>
2015-04-23 21:58:41 +0200

    Add StringClean

---
+++ Recursion-1/StringClean/StringClean/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringClean
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(StringClean("yyzzza"));
+            Console.WriteLine(StringClean("abbbcdd"));
+            Console.WriteLine(StringClean("Hello"));
+            Console.ReadLine();
+        }
+
+        private static string StringClean(string str)
+        {
+            if (str.Length < 2)
+                return str;
+            if (str[0].Equals(str[1]))
+                return StringClean(str.Substring(1));
+            return str[0] + StringClean(str.Substring(1));
+        }
+    }
+}
