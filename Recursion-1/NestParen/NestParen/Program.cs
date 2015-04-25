ae9af86cfe326f12fba250511c1c0cf8f96cb37d
Baha <baha.koyuncu@gmail.com>
2015-04-25 19:28:43 +0200

    Add NestParen

---
+++ Recursion-1/NestParen/NestParen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NestParen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NestParen("(())"));
+            Console.WriteLine(NestParen("((()))"));
+            Console.WriteLine(NestParen("(((x))"));
+            Console.ReadLine();
+        }
+
+        private static bool NestParen(string str)
+        {
+            if (str.Length < 1)
+                return true;
+            if (str[0].Equals('(') && str[str.Length - 1].Equals(')'))
+                return NestParen(str.Substring(1, str.Length - 2));
+            return false;
+        }
+    }
+}
