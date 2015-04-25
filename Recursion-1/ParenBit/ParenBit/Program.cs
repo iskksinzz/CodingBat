25bc0ed070032114182771b6c0d4885e15f9dea3
Baha <baha.koyuncu@gmail.com>
2015-04-25 15:09:42 +0200

    AddParenBit

---
+++ Recursion-1/ParenBit/ParenBit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParenBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParenBit("xyz(abc)123"));
+            Console.WriteLine(ParenBit("x(hello)"));
+            Console.WriteLine(ParenBit("(xy)1"));
+            Console.ReadLine();
+        }
+
+        private static string ParenBit(string str)
+        {
+            return str[0] == '(' ? str.Substring(0, str.IndexOf(')') + 1) : ParenBit(str.Substring(1));
+        }
+    }
+}
