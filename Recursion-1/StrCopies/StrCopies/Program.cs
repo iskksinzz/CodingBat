a4fe1be63eca5b021e002137958c81d24f3216d8
Baha <baha.koyuncu@gmail.com>
2015-04-25 19:29:49 +0200

    Add StrCopies

---
+++ Recursion-1/StrCopies/StrCopies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StrCopies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrCopies("catcowcat", "cat", 2));
+            Console.WriteLine(StrCopies("catcowcat", "cow", 2));
+            Console.WriteLine(StrCopies("catcowcat", "cow", 1));
+            Console.ReadLine();
+        }
+
+        private static bool StrCopies(string str, string sub, int n)
+        {
+            if (str.Length < sub.Length)
+                return n <= 0;
+            if (str.Substring(0, sub.Length).Equals(sub))
+                return StrCopies(str.Substring(1), sub, n - 1);
+            return StrCopies(str.Substring(1), sub, n);
+        }
+    }
+}
