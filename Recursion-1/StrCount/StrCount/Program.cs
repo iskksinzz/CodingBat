665b0b1a6c38175c637aeca1e9a47eb4d7d54355
Baha <baha.koyuncu@gmail.com>
2015-04-25 19:29:11 +0200

    Add StrCount

---
+++ Recursion-1/StrCount/StrCount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StrCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrCount("catcowcat", "cat"));
+            Console.WriteLine(StrCount("catcowcat", "cow"));
+            Console.WriteLine(StrCount("catcowcat", "dog"));
+            Console.ReadLine();
+        }
+
+        private static int StrCount(string str, string sub)
+        {
+            if (str.Length < sub.Length)
+                return 0;
+            if (str.Substring(0, sub.Length).Equals(sub))
+                return 1 + StrCount(str.Substring(sub.Length), sub);
+            return StrCount(str.Substring(1), sub);
+        }
+    }
+}
