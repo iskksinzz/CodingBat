e77ec87d3a7140613f1d0964e91167f5151fe64c
Baha <baha.koyuncu@gmail.com>
2015-04-26 11:51:42 +0200

    Add ScoresClump

---
+++ AP-1/ScoresClump/ScoresClump/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ScoresClump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ScoresClump(new[] { 3, 4, 5 }));
+            Console.WriteLine(ScoresClump(new[] { 3, 4, 6 }));
+            Console.WriteLine(ScoresClump(new[] { 1, 3, 5, 5 }));
+            Console.ReadLine();
+        }
+
+        private static bool ScoresClump(int[] scores)
+        {
+            for (var i = 0; i < scores.Length - 2; i++)
+                if (scores[i + 2] - scores[i] < 3)
+                    return true;
+            return false;
+        }
+    }
+}
