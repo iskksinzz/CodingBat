91911bb12311f9832950f5f41963d2560fc40acb
Baha <baha.koyuncu@gmail.com>
2015-04-26 11:51:16 +0200

    Add Scores100

---
+++ AP-1/Scores100/Scores100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Scores100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Scores100(new[] { 1, 100, 100 }));
+            Console.WriteLine(Scores100(new[] { 1, 100, 99, 100 }));
+            Console.WriteLine(Scores100(new[] { 100, 1, 100, 100 }));
+            Console.ReadLine();
+        }
+
+        private static bool Scores100(int[] scores)
+        {
+            for (var i = 0; i < scores.Length - 1; i++)
+                if (scores[i] == 100 && scores[i + 1] == 100)
+                    return true;
+            return false;
+        }
+    }
+}
