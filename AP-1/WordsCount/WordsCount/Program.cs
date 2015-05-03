90ac4ea288f4aa01e907016b9c3cbfd915b503fd
Baha <baha.koyuncu@gmail.com>
2015-05-03 17:48:02 +0200

    Add WordsCount

---
+++ AP-1/WordsCount/WordsCount/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordsCount(new[] { "a", "bb", "b", "ccc" }, 1));
+            Console.WriteLine(WordsCount(new[] { "a", "bb", "b", "ccc" }, 3));
+            Console.WriteLine(WordsCount(new[] { "a", "bb", "b", "ccc" }, 4));
+            Console.ReadLine();
+        }
+
+        private static int WordsCount(string[] words, int len)
+        {
+            return words.Count(word => word.Length == len);
+        }
+    }
+}
