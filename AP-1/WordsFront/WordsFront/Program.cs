c5ec98dd643d5e2f6fd7b66e30455b0465d66b93
Baha <baha.koyuncu@gmail.com>
2015-05-19 00:03:16 +0200

    Add WordsFront

---
+++ AP-1/WordsFront/WordsFront/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WordsFront
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordsFront(new[] { "a", "b", "c", "d" }, 1));
+            Console.WriteLine(WordsFront(new[] { "a", "b", "c", "d" }, 2));
+            Console.WriteLine(WordsFront(new[] { "a", "b", "c", "d" }, 3));
+            Console.ReadLine();
+        }
+
+        private static string[] WordsFront(string[] words, int n)
+        {
+            var arr = new string[n];
+            for (var i = 0; i < n; i++)
+                arr[i] = words[i];
+            return arr;
+        }
+    }
+}
