707a67e6ccf5a6ec00e90df2e187165183d85912
Baha <baha.koyuncu@gmail.com>
2015-05-20 00:12:32 +0200

    Add WordsWithoutList

---
+++ AP-1/WordsWithoutList/WordsWithoutList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordsWithoutList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordsWithoutList(new[] { "a", "bb", "b", "ccc" }, 1));
+            Console.WriteLine(WordsWithoutList(new[] { "a", "bb", "b", "ccc" }, 3));
+            Console.WriteLine(WordsWithoutList(new[] { "a", "bb", "b", "ccc" }, 4));
+            Console.ReadLine();
+        }
+
+        private static List<string> WordsWithoutList(string[] words, int len)
+        {
+            var list = new List<string>();
+            foreach (var word in words)
+                if (word.Length != len)
+                    list.Add(word);
+            return list;
+        }
+    }
+}
