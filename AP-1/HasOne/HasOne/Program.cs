dec8637075b60b85085165a5ba9dc9d47e8a0bcb
Baha <baha.koyuncu@gmail.com>
2015-05-20 00:27:02 +0200

    Add HasOne

---
+++ AP-1/HasOne/HasOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HasOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasOne(10));
+            Console.WriteLine(HasOne(22));
+            Console.WriteLine(HasOne(220));
+            Console.ReadLine();
+        }
+
+        private static bool HasOne(int n)
+        {
+            return n != 0 && (n % 10 == 1 || HasOne(n / 10));
+        }
+    }
+}
