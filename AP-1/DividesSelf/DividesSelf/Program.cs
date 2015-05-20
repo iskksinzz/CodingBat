183cc66332a59dcf506b8ab522e7c6ff2669588c
Baha <baha.koyuncu@gmail.com>
2015-05-20 23:24:55 +0200

    Add DivideSelf

---
+++ AP-1/DividesSelf/DividesSelf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DividesSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DividesSelf(128));
+            Console.WriteLine(DividesSelf(12));
+            Console.WriteLine(DividesSelf(120));
+            Console.ReadLine();
+        }
+
+        private static bool DividesSelf(int n)
+        {
+            var val = n;
+            while (val > 0)
+            {
+                if (val % 10 == 0 || n % (val % 10) != 0)
+                    return false;
+                val /= 10;
+            }
+            return true;
+        }
+    }
+}
