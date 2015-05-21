a485c7261727a91d16232004aec30f25176b02a3
Baha <baha.koyuncu@gmail.com>
2015-05-21 18:45:46 +0200

    Add CopyEvens

---
+++ AP-1/CopyEvens/CopyEvens/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CopyEvens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopyEvens(new[] { 3, 2, 4, 5, 8 }, 2);
+            CopyEvens(new[] { 3, 2, 4, 5, 8 }, 3);
+            CopyEvens(new[] { 6, 1, 2, 4, 5, 8 }, 3);
+            Console.ReadLine();
+        }
+
+        private static int[] CopyEvens(int[] nums, int count)
+        {
+            var arr = new int[count];
+            var j = 0;
+            foreach (var num in nums)
+            {
+                if (count == j) { break; }
+                if (num % 2 == 0)
+                    arr[j++] = num;
+            }
+            return arr;
+        }
+    }
+}
