1adea1f416f317dedf26254c5091a5d0c0027a30
Baha <baha.koyuncu@gmail.com>
2015-05-23 12:16:03 +0200

    Add ScoreUp

---
+++ AP-1/ScoreUp/ScoreUp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ScoreUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ScoreUp(new[] { "a", "a", "b", "b" }, new[] { "a", "c", "b", "c" }));
+            Console.WriteLine(ScoreUp(new[] { "a", "a", "b", "b" }, new[] { "a", "a", "b", "c" }));
+            Console.WriteLine(ScoreUp(new[] { "a", "a", "b", "b" }, new[] { "a", "a", "b", "b" }));
+            Console.ReadLine();
+        }
+
+        private static int ScoreUp(string[] key, string[] answers)
+        {
+            var score = 0;
+            for (var i = 0; i < answers.Length; i++)
+            {
+                if (key[i].Equals(answers[i]))
+                    score += 4;
+                else if (!answers[i].Equals("?"))
+                    score -= 1;
+            }
+            return score;
+        }
+    }
+}
