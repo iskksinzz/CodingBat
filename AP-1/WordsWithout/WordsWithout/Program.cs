9b27c853fe89c6aad0a3b60f407b875057b1fc5c
Baha <baha.koyuncu@gmail.com>
2015-05-23 12:16:21 +0200

    Add WordsWithout

---
+++ AP-1/WordsWithout/WordsWithout/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsWithout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordsWithout(new[] { "a", "b", "c", "a" }, "a"));
+            Console.WriteLine(WordsWithout(new[] { "a", "b", "c", "a" }, "b"));
+            Console.WriteLine(WordsWithout(new[] { "a", "b", "c", "a" }, "c"));
+            Console.ReadLine();
+        }
+
+        private static string[] WordsWithout(string[] words, string target)
+        {
+
+            var arr = new string[words.Count(str => !str.Equals(target))];
+            var j = 0;
+            foreach (var str in words.Where(str => !str.Equals(target)))
+                arr[j++] = str;
+            return arr;
+        }
+    }
+}
