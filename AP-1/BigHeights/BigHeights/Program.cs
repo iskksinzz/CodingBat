8e4cd3a043e5a763d168d50ec248e93ac9b9208e
Baha <baha.koyuncu@gmail.com>
2015-05-25 12:51:22 +0200

    Add BigHeights

---
+++ AP-1/BigHeights/BigHeights/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BigHeights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigHeights(new[] { 5, 3, 6, 7, 2 }, 2, 4));
+            Console.WriteLine(BigHeights(new[] { 5, 3, 6, 7, 2 }, 0, 1));
+            Console.WriteLine(BigHeights(new[] { 5, 3, 6, 7, 2 }, 0, 4));
+            Console.ReadLine();
+        }
+
+        private static int BigHeights(int[] heights, int start, int end)
+        {
+            var sum = 0;
+            for (var i = start; i < end; i++)
+                if (Math.Abs(heights[i] - heights[i + 1]) >= 5)
+                    sum++;
+            return sum;
+        }
+    }
+}
