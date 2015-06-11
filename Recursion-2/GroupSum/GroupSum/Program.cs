1f4c333ea80241e79932e17ac5648d0bfd47bd53
bahagiga <bahagiga@users.noreply.github.com>
2015-06-11 22:54:39 +0200

    Add GroupSum

---
+++ Recursion-2/GroupSum/GroupSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GroupSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GroupSum(0, new[] { 2, 4, 8 }, 10));
+            Console.ReadLine();
+        }
+
+        private static bool GroupSum(int start, int[] nums, int target)
+        {
+            if (start >= nums.Length)
+                return target == 0;
+            if (GroupSum(start + 1, nums, target - nums[start]))
+                return true;
+            if (GroupSum(start + 1, nums, target))
+                return true;
+            return false;
+        }
+    }
+}
