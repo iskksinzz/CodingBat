4570a024d6d362f0adbe1a51adf6117f23cb297c
Baha <baha.koyuncu@gmail.com>
2015-09-08 20:56:18 +0200

    Add GroupNoAdj

---
+++ Recursion-2/GroupNoAdj/GroupNoAdj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GroupSumClump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GroupNoAdj(0, new[] { 2, 5, 10, 4 }, 12));
+            Console.WriteLine(GroupNoAdj(0, new[] { 2, 5, 10, 4 }, 14));
+            Console.WriteLine(GroupNoAdj(0, new[] { 2, 5, 10, 4 }, 7));
+            Console.ReadLine();
+        }
+
+        private static bool GroupNoAdj(int start, int[] nums, int target)
+        {
+            if (start >= nums.Length)
+                return target == 0;
+            return GroupNoAdj(start + 2, nums, target - nums[start]) || GroupNoAdj(start + 1, nums, target);
+        }
+    }
+}
